dcd00bba77da2c850b79d48e9bd4b4c3b9d1d50e
Tyrinslys Valinlore <tyrinslys@gmail.com>
2016-03-25 14:04:51 -0500

    refactor test to have more detail

--- src/test/java/com/valinlore/kata/vending/domain/VendingMachineTest.java
+++ src/test/java/com/valinlore/kata/vending/domain/VendingMachineTest.java
@@ -14,28 +14,32 @@ import static org.hamcrest.Matchers.*;
  */
 public class VendingMachineTest {
 
+	private static final String INSERT_COIN = "INSERT COIN";
+
 	/**
 	 * This test is for the user story titled "Accept Coins"
 	 */
 	@Test
 	public void acceptCoins_testThatDisplayWithNoCoins() {
-		// Given:
+		// Given: a vending machine with no coins
 		VendingMachine vendingMachine = new VendingMachine();
-		// When:
-		// no coins added.
-		// Then:
-		assertThat(vendingMachine.getDisplay(), is(VendingMachine.DEFAULT_DISPLAY));
+		// When: no coins have been added.
+		// Then: insert coin message is displayed
+		assertThat(vendingMachine.getDisplay(), is(INSERT_COIN));
 	}
 
 	@Test
 	public void acceptCoins_testThatAccepQuarters() {
-		// Given:
-		VendingMachine vendingMachine = new VendingMachine();
+		//Setup: a coin for future use
 		Coin coin = createCoin(AcceptedCoinTypes.QUARTER);
-		// When:
+		
+		// Given: a vending machine with no coins.
+		VendingMachine vendingMachine = new VendingMachine();
+		// When: insert a coin
 		vendingMachine.insert(coin );
-		// Then:
-		assertThat(vendingMachine.getDisplay(), not(VendingMachine.DEFAULT_DISPLAY));
+		// Then: Insert coin message goes away
+		assertThat(vendingMachine.getDisplay(), not(INSERT_COIN));
+		// and: coin is accepted (no coins in the coin return)
 		assertThat(vendingMachine.peekCoinReturn(), emptyCollectionOf(Coin.class));
 	}
 
