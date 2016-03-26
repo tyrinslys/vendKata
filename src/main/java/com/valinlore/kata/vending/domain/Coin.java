6ce486408a4f35c5827f5eeb296f1811f5d543e3
Tyrinslys Valinlore <tyrinslys@gmail.com>
2016-03-26 10:20:35 -0400

    adding code to pass the broken test

--- src/main/java/com/valinlore/kata/vending/domain/Coin.java
+++ src/main/java/com/valinlore/kata/vending/domain/Coin.java
@@ -1,14 +1,25 @@
 package com.valinlore.kata.vending.domain;
 
 public class Coin {
-	int weight;
-	int size;
+	private final int weight;
+	private final int size;
 
 	/**
-	 * @param weight In miligrams
-	 * @param size in millimeters
+	 * @param weight
+	 *            In miligrams
+	 * @param size
+	 *            in micrometers
 	 */
 	public Coin(int weight, int size) {
+		this.weight = weight;
+		this.size = size;
+	}
 
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getSize() {
+		return size;
 	}
 }
--- src/main/java/com/valinlore/kata/vending/domain/CoinUtils.java
+++ src/main/java/com/valinlore/kata/vending/domain/CoinUtils.java
@@ -1,8 +1,46 @@
 package com.valinlore.kata.vending.domain;
 
+import org.apache.commons.lang3.Range;
+
 public class CoinUtils {
-	public static AcceptedCoinTypes determineCoinType(Coin coin){
-		return AcceptedCoinTypes.QUARTER;
+	private static final Range<Integer> QUARTER_SIZE_RANGE;
+	private static final Range<Integer> DIME_SIZE_RANGE;
+	private static final Range<Integer> NICKEL_SIZE_RANGE;
+
+	static {
+		{ // setup for quarter
+			int perfectQuarterSize = AcceptedCoinTypes.QUARTER.getSizeInMicroMeters();
+			int quarterSizeTolerance = AcceptedCoinTypes.QUARTER.getSizeTolerance();
+			QUARTER_SIZE_RANGE = Range.between(perfectQuarterSize - quarterSizeTolerance,
+					perfectQuarterSize + quarterSizeTolerance);
+		}
+		{ // setup for dime
+			int perfectDimeSize = AcceptedCoinTypes.DIME.getSizeInMicroMeters();
+			int dimeSizeTolerance = AcceptedCoinTypes.DIME.getSizeTolerance();
+			DIME_SIZE_RANGE = Range.between(perfectDimeSize - dimeSizeTolerance, perfectDimeSize + dimeSizeTolerance);
+		}
+		{ // setup for nickel
+			int perfectNickelSize = AcceptedCoinTypes.NICKEL.getSizeInMicroMeters();
+			int nickelSizeTolerance = AcceptedCoinTypes.NICKEL.getSizeTolerance();
+			NICKEL_SIZE_RANGE = Range.between(perfectNickelSize - nickelSizeTolerance,
+					perfectNickelSize + nickelSizeTolerance);
+		}
 	}
 
+	public static AcceptedCoinTypes determineCoinType(Coin coin) {
+		AcceptedCoinTypes sizeFits = checkSize(coin);
+		return sizeFits;
+	}
+
+	private static AcceptedCoinTypes checkSize(Coin coin) {
+		if (QUARTER_SIZE_RANGE.contains(coin.getSize())) {
+			return AcceptedCoinTypes.QUARTER;
+		} else if (DIME_SIZE_RANGE.contains(coin.getSize())) {
+			return AcceptedCoinTypes.DIME;
+		} else if (NICKEL_SIZE_RANGE.contains(coin.getSize())) {
+			return AcceptedCoinTypes.NICKEL;
+		} else {
+			return null;
+		}
+	}
 }
