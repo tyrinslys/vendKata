938325c3c6ba4e3b93404fd621059de1df090cb6
Tyrinslys Valinlore <tyrinslys@gmail.com>
2016-04-02 09:24:35 -0400

    adding more tests and fixing comments

--- src/test/java/com/valinlore/kata/vending/domain/VendingMachine_UserStory_ExactChangeTest.java
+++ src/test/java/com/valinlore/kata/vending/domain/VendingMachine_UserStory_ExactChangeTest.java
@@ -23,7 +23,7 @@ public class VendingMachine_UserStory_ExactChangeTest {
 
 	@Test
 	public void testExactChangeMessage_WithNoChange() {
-		// Given: a vending machine with no coins and no change
+		// Given: a vending machine with no change
 		VendingMachine vendingMachine = new VendingMachine();
 		
 		// When: default state... nothing to do here
@@ -35,7 +35,7 @@ public class VendingMachine_UserStory_ExactChangeTest {
 	}
 	@Test
 	public void testExactChangeMessage_WithNotEnoughChange_case1() {
-		// Given: a vending machine with no coins and 1 nickel
+		// Given: a vending machine with 1 nickel
 		VendingMachine vendingMachine = new VendingMachine();
 		addChange(vendingMachine, AcceptedCoinTypes.NICKEL, 1);
 		
@@ -48,7 +48,7 @@ public class VendingMachine_UserStory_ExactChangeTest {
 	}
 	@Test
 	public void testExactChangeMessage_WithNotEnoughChange_case2() {
-		// Given: a vending machine with no coins and 1 nickel
+		// Given: a vending machine with 1 dime and 1 nickel
 		VendingMachine vendingMachine = new VendingMachine();
 		addChange(vendingMachine, AcceptedCoinTypes.DIME, 1);
 		addChange(vendingMachine, AcceptedCoinTypes.NICKEL, 1);
@@ -61,31 +61,58 @@ public class VendingMachine_UserStory_ExactChangeTest {
 		assertThat(vendingMachine.viewDisplay(), is(EXACT_CHANGE_ONLY));
 	}
 	@Test
+	public void testExactChangeMessage_WithNotEnoughChange_case3() {
+		// Given: a vending machine with 2 nickel
+		VendingMachine vendingMachine = new VendingMachine();
+		addChange(vendingMachine, AcceptedCoinTypes.NICKEL, 2);
+		
+		// When: default state... nothing to do here
+
+		// Then: exact change is displayed
+		assertThat(vendingMachine.viewDisplay(), is(EXACT_CHANGE_ONLY));
+		// and: on second look display is still exact change
+		assertThat(vendingMachine.viewDisplay(), is(EXACT_CHANGE_ONLY));
+	}
+	@Test
+	public void testExactChangeMessage_WithNotEnoughChange_case4() {
+		// Given: a vending machine with 3 nickel
+		VendingMachine vendingMachine = new VendingMachine();
+		addChange(vendingMachine, AcceptedCoinTypes.NICKEL, 3);
+		
+		// When: default state... nothing to do here
+
+		// Then: exact change is displayed
+		assertThat(vendingMachine.viewDisplay(), is(EXACT_CHANGE_ONLY));
+		// and: on second look display is still exact change
+		assertThat(vendingMachine.viewDisplay(), is(EXACT_CHANGE_ONLY));
+	}
+
+	@Test
 	public void testExactChangeMessage_WithJustEnoughChange_case1() {
-		// Given: a vending machine with no coins and 2 dimes
+		// Given: a vending machine with 2 dimes and 1 nickel
 		VendingMachine vendingMachine = new VendingMachine();
 		addChange(vendingMachine, AcceptedCoinTypes.DIME, 2);
 		addChange(vendingMachine, AcceptedCoinTypes.NICKEL, 1);
 		
 		// When: default state... nothing to do here
 
-		// Then: exact change is displayed
+		// Then: defalut message is displayed
 		assertThat(vendingMachine.viewDisplay(), is(INSERT_COIN));
-		// and: on second look display is still exact change
+		// and: on second look display is still default message
 		assertThat(vendingMachine.viewDisplay(), is(INSERT_COIN));
 	}
 	@Test
 	public void testExactChangeMessage_WithJustEnoughChange_case2() {
-		// Given: a vending machine with no coins and 2 dimes
+		// Given: a vending machine with 1 dimes and 2 nickel
 		VendingMachine vendingMachine = new VendingMachine();
 		addChange(vendingMachine, AcceptedCoinTypes.DIME, 1);
 		addChange(vendingMachine, AcceptedCoinTypes.NICKEL, 2);
 		
 		// When: default state... nothing to do here
 
-		// Then: exact change is displayed
+		// Then: default message is displayed
 		assertThat(vendingMachine.viewDisplay(), is(INSERT_COIN));
-		// and: on second look display is still exact change
+		// and: on second look display is still default message
 		assertThat(vendingMachine.viewDisplay(), is(INSERT_COIN));
 	}
 }
